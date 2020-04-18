3124970da510ae94c2007d9342db4cadd6f67042
YashikaNarasaraj <yashika.narasaraj@gmail.com>
2020-04-17 22:12:09 -0400

    registration comp linked to app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './public/login/login.component';
+import { RegistrationComponent } from './public/registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
