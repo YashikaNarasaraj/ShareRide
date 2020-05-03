ed28323dd7efdf52a90b01a00aa2d76817f0f1fd
1prawin <praveenkumar1619.pk@gmail.com>
2020-05-03 14:53:48 -0400

    AboutUs content development

--- src/app/secure/profile/profile.component.spec.ts
+++ src/app/secure/profile/profile.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FeaturesComponent } from './features.component';
+import { ProfileComponent } from './profile.component';
 
-describe('FeaturesComponent', () => {
-  let component: FeaturesComponent;
-  let fixture: ComponentFixture<FeaturesComponent>;
+describe('ProfileComponent', () => {
+  let component: ProfileComponent;
+  let fixture: ComponentFixture<ProfileComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FeaturesComponent ]
+      declarations: [ ProfileComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(FeaturesComponent);
+    fixture = TestBed.createComponent(ProfileComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/secure/secure.module.ts
+++ src/app/secure/secure.module.ts
@@ -8,11 +8,13 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 const appRoutes: Routes = [
     { path : '', component : SecureComponent, children : [
-        { path : '', component : HomeComponent}, 
+        { path : '', component : AboutUsComponent}, 
         {path : 'postride', component : PostrideComponent},
         {path : 'findride', component : FindrideComponent},
         {path : 'requestRide', component : RequestRideComponent},
-        {path : 'rideRequests', component : RideRequestsComponent}]}
+        {path : 'rideRequests', component : RideRequestsComponent},
+        {path : 'AboutUs', component : AboutUsComponent},
+        {path : 'profile', component : ProfileComponent}]}
 ]
 
 
@@ -25,7 +27,7 @@ import { RequestRideComponent } from './request-ride/request-ride.component';
 import { RideRequestsComponent } from './ride-requests/ride-requests.component';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
-import { FeaturesComponent } from './features/features.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 
@@ -40,7 +42,7 @@ import { FeaturesComponent } from './features/features.component';
     RequestRideComponent,
     RideRequestsComponent,
     AboutUsComponent,
-    FeaturesComponent
+    ProfileComponent,
   ],
   imports: [
     RouterModule.forChild(appRoutes),
