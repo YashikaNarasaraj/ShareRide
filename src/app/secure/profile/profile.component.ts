4e116e0b5b8cc95844668e4d38ab75698ef8a3d9
1prawin <praveenkumar1619.pk@gmail.com>
2020-05-05 16:41:46 -0400

    profile page designed

--- src/app/secure/profile/profile.component.ts
+++ src/app/secure/profile/profile.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { faRoad } from '@fortawesome/free-solid-svg-icons';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import { faCar } from '@fortawesome/free-solid-svg-icons';
+import { faCalendar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-profile',
@@ -6,6 +11,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+
+  faRoad = faRoad;
+  faHome = faHome;
+  faThumbsUp = faThumbsUp;
+  faCar = faCar;
+  faCalendar = faCalendar;
 
   constructor() { }
 
